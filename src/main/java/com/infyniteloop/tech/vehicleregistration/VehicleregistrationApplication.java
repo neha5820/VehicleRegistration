930910e037e821422660e8c467ab25f727745c26
Neha <er.neha.singh@gmail.com>
2022-01-29 16:47:31 +1100

    Initial Commoit

---
+++ src/main/java/com/infyniteloop/tech/vehicleregistration/VehicleregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.infyniteloop.tech.vehicleregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infyniteloop/tech/vehicleregistration/VehicleregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infyniteloop.tech.vehicleregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
